30a9e194ef13c767893dbea98c31c6645a898b80
nvminh0810 <nvminh.github@gmail.com>
2022-01-24 15:47:52 +0700

    add dockerfile to build image

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ app.use("/groups", verifyToken, groupRoute);
 
 app.use(errorHandler);
 
-app.listen(3000, () => console.log(`Server is running on port: ${3000}`));
+app.listen(8080, () => console.log(`Server is running on port: ${8080}`));
